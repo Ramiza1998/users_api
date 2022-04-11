2313619d3c4c2d3638277bf37e81614405d3abfa
Ramiza1998 <ramizayakubu994@gmail.com>
2022-04-11 04:23:26 -0700

    added some new data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =require("express")
 const userRoute = require("./routes/usersRoutes")
+const path= require ("path");
+
 
 const app = express();
 
@@ -11,7 +13,11 @@ app.use(userRoute)
 
 //home route 
 app.get("/", (req, res)=>{
-    res.status(200).send("<h1>WELCOME TO THE USERS DATABASE</h1>");
+    res.sendFile(path.join(__dirname + "/pages/index.html"))
+})
+
+app.get("/*", (req, res)=>{
+    res.status(400).sendFile(path.join(__dirname + "/pages/404.html"))
 })
 
 
